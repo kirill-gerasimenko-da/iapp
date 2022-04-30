49aaf573fb580f7423dbfd4972fb7562631c35cf
kir <kir@your-mail.com>
2022-04-30 14:16:02 +0200

    use clojure 11 funcs; minors

--- src/iapp/core.clj
+++ src/iapp/core.clj
@@ -7,12 +7,6 @@
   (:import [javafx.scene.input Clipboard]
            [javafx.application Platform]))
 
-(defn parse-number
-  "Reads a number from a string. Returns nil if not a number."
-  [s]
-  (when (re-find #"^-?\d+\.?\d*$" s)
-    (read-string s)))
-
 (defn get-clipboard-text []
   @(fx/on-fx-thread
     (let [cb (.getString (Clipboard/getSystemClipboard))]
@@ -28,19 +22,13 @@
                   4 {:col 4 :slot nil :value nil}}}
          cache/lru-cache-factory)))
 
-;; monitor state changes
-;; (add-watch *app-context nil #(pprint (:cljfx.context/m %4)))
-
 (defmulti event-handler :event/type)
 (defmethod event-handler :default [e] (pprint e))
 
 (defn stage [_]
   {:fx/type :stage
    :showing true
-   ;; :x 1300
-   ;; :y 300
    :resizable false
-   ;; :always-on-top true
    :icons ["iapp/logo.png"]
    :title "Password util"
    :scene {:fx/type :scene :root {:fx/type root/component}}})
@@ -49,23 +37,26 @@
   (when-some [s slot]
     (str (get password (- s 1) nil))))
 
+(defn- update-password-slots [password slots]
+  (update-vals
+   slots
+   (fn [val] (assoc val :value (get-slot-value password (:slot val))))))
+
 (defmethod event-handler :ev/set-password [{:keys [fx/event fx/context]}]
   (let [password event
         slots    (->> (fx/sub-val context :slots)
-                      (map (fn [[_ {:keys [col slot]}]] [col {:col col :slot slot :value (get-slot-value password slot)}]))
-                      (into {}))]
+                      (update-password-slots password))]
     {:context (fx/swap-context context merge {:password password :slots slots})}))
 
 (defmethod event-handler :ev/paste-password [{:keys [fx/context]}]
   (let [password (get-clipboard-text)
         slots    (->> (fx/sub-val context :slots)
-                      (map (fn [[_ {:keys [col slot]}]] [col {:col col :slot slot :value (get-slot-value password slot)}]))
-                      (into {}))]
+                      (update-password-slots password))]
     {:context (fx/swap-context context merge {:password password :slots slots})}))
 
 (defmethod event-handler :ev/set-slot-value [{:keys [fx/event fx/context col]}]
   (let [password (fx/sub-val context :password)
-        slot     (parse-number event)
+        slot     (parse-long event)
         value    (get-slot-value password slot)]
     {:context (fx/swap-context context assoc-in [:slots col] {:col col :slot slot :value value})}))
 
